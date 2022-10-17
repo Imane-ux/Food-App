e86cc7353c6d4beba3586fff4c3c9cdb3044651d
jnnoah <jnnoah2211@gmail.com>
2022-10-17 12:23:45 -0400

    Create User.java

---
+++ app/src/main/java/com/example/mealyapp/User.java
@@ -0,0 +1,23 @@
+package com.example.mealyapp;
+
+public class User {
+    public String password,email,firstName,lastName,address,cardNumber,expiryYear,expiryMonth,securityCode,nameOnCard;
+
+    public User(String password, String email, String firstName, String lastName, String address, String cardNumber, String expiryYear, String expiryMonth, String securityCode, String nameOnCard) {
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.cardNumber = cardNumber;
+        this.expiryYear = expiryYear;
+        this.expiryMonth = expiryMonth;
+        this.securityCode = securityCode;
+        this.nameOnCard = nameOnCard;
+    }
+
+    public User(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
+}
