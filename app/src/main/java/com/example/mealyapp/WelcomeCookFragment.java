d140bb1fdecde649d995e76a8c8b202ded4b186d
jnnoah <jnnoah2211@gmail.com>
2022-11-01 14:44:27 -0400

    Update WelcomeCookFragment.java

--- app/src/main/java/com/example/mealyapp/WelcomeCookFragment.java
+++ app/src/main/java/com/example/mealyapp/WelcomeCookFragment.java
@@ -1,12 +1,6 @@
 package com.example.mealyapp;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +8,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class WelcomeCookFragment extends Fragment {
     private FirebaseAuth mAuth;
@@ -28,29 +30,19 @@ public class WelcomeCookFragment extends Fragment {
         if ((mAuth.getCurrentUser().getUid()).equals("40ylmmzjcSby42d0bMxG2ZO8EX12")){
             String a= getString(R.string.wel_admin);
             tV.setText(a);
+
+            Timer timer = new Timer();
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, new AdminFragment()).commit();
+                }
+            };
+
+            timer.schedule(timerTask, 1000);
+
         }else{
-            /*FirebaseUser user = mAuth.getCurrentUser();
-            FirebaseDatabase.getInstance().getReference("user").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                    User userProfile = snapshot.getValue(User.class);
-                    if (userProfile != null){
-                        String role= userProfile.role;
-                        if (role.equals("Cook")){
-                        tV.setText(getString(R.string.wel_cook));
-                        }else if (role.equals( "Client")) {
-                            tV.setText(getString(R.string.wel_client));
-                        }
-
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });*/
             tV.setText(getString(R.string.wel_cook));
         }
 
