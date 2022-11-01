238dc5b43e0c140d305486b8cdda399ce48039f4
jnnoah <jnnoah2211@gmail.com>
2022-11-01 14:44:19 -0400

    Update AdminFragment.java

--- app/src/main/java/com/example/mealyapp/AdminFragment.java
+++ app/src/main/java/com/example/mealyapp/AdminFragment.java
@@ -1,6 +1,10 @@
 package com.example.mealyapp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -8,14 +12,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-//import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
