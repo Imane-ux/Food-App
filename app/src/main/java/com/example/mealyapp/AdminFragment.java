2f9abafd3c96bf74bcb513f5c402b7d0c358b7da
NidaTaj <ntaj055@uottawa.ca>
2022-11-02 11:15:13 -0400

    added method to delete from firebase

--- app/src/main/java/com/example/mealyapp/AdminFragment.java
+++ app/src/main/java/com/example/mealyapp/AdminFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,18 +29,26 @@ public class AdminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =inflater.inflate(R.layout.fragment_admin, container, false);
+
+        /*recyclerView= getActivity().findViewById(R.id.recyclerView1);
+        ref= FirebaseDatabase.getInstance().getReference().child("Complaints");
+        layoutManager= new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);*/
+
+
+
+
         return v;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        recyclerView= view.findViewById(R.id.recyclerView1);
-        ref= FirebaseDatabase.getInstance().getReference().child("Complaints");
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView= getActivity().findViewById(R.id.recyclerView1);
         recyclerView.setHasFixedSize(true);
-        layoutManager= new LinearLayoutManager(getContext());
+        ref= FirebaseDatabase.getInstance().getReference().child("Complaints");
+        layoutManager= new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-
     }
 
     @Override
@@ -78,6 +87,13 @@ public class AdminFragment extends Fragment {
                 itemComplaint= itemView.findViewById(R.id.ismComplaint);
             }
         }
+        public boolean deleteComplaint(String id){
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("Complaints").child(id);
+        dR.removeValue();
+            Toast.makeText(getActivity(), "Complaint Actioned", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
 
     }
 //}
