fc6df12fb249d8ced722f57fc049773dd67072c8
jnnoah <jnnoah2211@gmail.com>
2022-11-04 18:53:44 -0400

    Create ComplaintTest.java

---
+++ app/src/test/java/com/example/mealyapp/ComplaintTest.java
@@ -0,0 +1,16 @@
+package com.example.mealyapp;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class ComplaintTest extends TestCase {
+
+    @Test
+    public void testGetComplaint() {
+        Complaint complaint = new Complaint("Bad");
+        String expected = "Bad";
+
+        assertEquals(expected, complaint.getComplaint());
+    }
+}
