3afe5b27a3c70a070ca71164edbed1929a53db37
jnnoah <jnnoah2211@gmail.com>
2022-11-04 18:53:37 -0400

    Create UserTest.java

---
+++ app/src/test/java/com/example/mealyapp/UserTest.java
@@ -0,0 +1,20 @@
+package com.example.mealyapp;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class UserTest extends TestCase {
+
+    @Test
+    public void testGetRole() {
+        User user = new User("Client");
+        User user2 = new User("Cook");
+        String expected = "Client";
+        String expected2 = "Cook";
+
+        assertEquals(expected, user.getRole());
+        assertEquals(expected2, user2.getRole());
+    }
+
+}
